0b255d9068a7be044c0a39e82673db8efcbf3a09
Arad Work PC <aradaryanbusiness@gmail.com>
2022-11-09 10:38:58 +0330

    JWT One Step Closer

--- Market.Presentation/Program.cs
+++ Market.Presentation/Program.cs
@@ -58,43 +58,6 @@ namespace Market.Presentation
 
             var app = builder.Build();
 
-            app.MapGet("/security/getMessage",
-                () => "Hello World!").RequireAuthorization();
-
-            app.MapPost("/security/createToken", [AllowAnonymous] (User user) =>
-            {
-                if (user.UserName == "sls123" && user.Password == "sls123")
-                {
-                    var issuer = builder.Configuration["Jwt:Issuer"];
-                    var audience = builder.Configuration["Jwt:Audience"];
-                    var key = Encoding.ASCII.GetBytes
-                    (builder.Configuration["Jwt:Key"]);
-                    var tokenDescriptor = new SecurityTokenDescriptor
-                    {
-                        Subject = new ClaimsIdentity(new[]
-                        {
-                new Claim("Id", Guid.NewGuid().ToString()),
-                new Claim(JwtRegisteredClaimNames.Sub, user.UserName),
-                new Claim(JwtRegisteredClaimNames.Email, user.UserName),
-                new Claim(JwtRegisteredClaimNames.Jti,
-                Guid.NewGuid().ToString())
-             }),
-                        Expires = DateTime.UtcNow.AddMinutes(5),
-                        Issuer = issuer,
-                        Audience = audience,
-                        SigningCredentials = new SigningCredentials
-                        (new SymmetricSecurityKey(key),
-                        SecurityAlgorithms.HmacSha512Signature)
-                    };
-                    var tokenHandler = new JwtSecurityTokenHandler();
-                    var token = tokenHandler.CreateToken(tokenDescriptor);
-                    var jwtToken = tokenHandler.WriteToken(token);
-                    var stringToken = tokenHandler.WriteToken(token);
-                    return Results.Ok(stringToken);
-                }
-                return Results.Unauthorized();
-            });
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -113,11 +76,5 @@ namespace Market.Presentation
             app.Run();
 
         }
-    }
-
-    public class User
-    {
-        public string UserName { get; set; }
-        public string Password { get; set; }
     }
 }
