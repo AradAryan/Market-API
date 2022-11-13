0eb6132e6f17bc71d4c9cdb82286adce6adbccbc
Arad Work PC <aradaryanbusiness@gmail.com>
2022-11-13 17:34:35 +0330

    Making Some Changes

--- Market.Domain/Models/Brand.cs
+++ Market.Domain/Models/Brand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Market.Domain.Models.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +9,7 @@ namespace Market.Domain.Models
 {
     //Done
     //TODO Brands have name , description , id 
-    public class Brand
+    public class Brand : BaseModel
     {
         public string? Name { get; set; }
         public string? description { get; set; }
